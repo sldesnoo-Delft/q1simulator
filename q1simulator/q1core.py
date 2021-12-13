6caf1ce153028760471a1e9f00c789927c6d0f00
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2021-12-13 09:59:27 +0100

    Improved asm argument checking and error reporting

--- q1simulator/q1core.py
+++ q1simulator/q1core.py
@@ -11,23 +11,6 @@ class Instruction:
     args: Optional[Tuple[str]] = None
     label: Optional[str] = None
 
-
-def evaluate_args(_func=None, *, dest_arg=None):
-
-    def decorator_args(func):
-        @wraps(func)
-        def func_wrapper(self, *args, **kwargs):
-            # print(f'{func.__name__} {args}')
-            args = self._evaluate_args(dest_arg, args)
-            return func(self, *args)
-
-        return func_wrapper
-
-    if _func is None:
-        return decorator_args
-    else:
-        return decorator_args(_func)
-
 class Halt(Exception):
     pass
 
@@ -36,6 +19,28 @@ class Abort(Exception):
 
 class Illegal(Exception):
     pass
+
+class AsmSyntaxError(Exception):
+    pass
+
+def evaluate_args(arg_types, *, dest_arg=None):
+
+    def decorator_args(func):
+        @wraps(func)
+        def func_wrapper(self, *args, **kwargs):
+            try:
+                # print(f'{func.__name__} {args}')
+                args = self._evaluate_args(arg_types, dest_arg, args)
+            except Exception as ex:
+                print(f'*** Exception: {ex}')
+                print(f'*** Error in instruction {func.__name__[1:]} {",".join(args)}')
+                print(f'*** Argument types: ({arg_types})')
+                raise
+            return func(self, *args)
+
+        return func_wrapper
+
+    return decorator_args
 
 
 
@@ -132,6 +137,15 @@ class Q1Core:
             msg = f'Execution aborted: {ex.args[0]}'
             self._print_error_msg(msg, instr, cntr)
             self._error(ex.args[1])
+        except AsmSyntaxError as ex:
+            msg = f'Syntax error: {ex.args[0]}'
+            self._print_error_msg(msg, instr, cntr)
+            self._error('SEQUENCE PROCESSOR Q1 ILLEGAL INSTRUCTION')
+        except:
+            self._print_error_msg('Exception', instr, cntr)
+            self._error('OOPS!!')
+            raise
+
 
     def _print_error_msg(self, msg, instr, cntr):
         last_line = self.lines[instr.text_line_nr]
@@ -142,29 +156,49 @@ class Q1Core:
     def _error(self, msg):
         self.errors.add(msg)
 
-    def _evaluate_args(self, dest_arg, args):
+    def _evaluate_args(self, arg_types, dest_arg, args):
         args = list(args)
+        types = arg_types.split(',') if arg_types else []
+        if len(args) != len(types):
+            raise AsmSyntaxError(f'Incorrect number of arguments {len(args)}<>{len(types)}')
+        select_imm = False
+        allow_imm = True
         for i,arg in enumerate(args):
+            allowed = types[i]
             c = arg[0]
             if i == dest_arg:
                 if c != 'R':
-                    raise Exception('Expected register argument')
+                    raise AsmSyntaxError('Destination must be register')
                 reg_nr = int(arg[1:])
                 args[i] = reg_nr
             elif c == '@':
+                if 'L' not in allowed:
+                    raise AsmSyntaxError(f'Label operand not support as argument {i}')
+                if 'R' in allowed:
+                    select_imm = True
                 try:
                     line_nr = self.labels[arg[1:]]
                     args[i] = line_nr
                 except:
-                    raise Exception(f'Label {arg} not defined')
+                    raise AsmSyntaxError(f'Label {arg} not defined')
             elif c == 'R':
+                if 'R' not in allowed:
+                    raise AsmSyntaxError(f'Register operand not support as argument {i}')
+                if 'I' in allowed:
+                    allow_imm = False
                 # add 1 clock tick for every register
                 self.clock.add_ticks(1)
                 reg_nr = int(arg[1:])
                 args[i] = self.R[reg_nr]
             else:
+                if 'I' not in allowed:
+                    raise AsmSyntaxError(f'Immediate operand not support as argument {i}')
+                if 'R' in allowed:
+                    select_imm = True
                 args[i] = int(arg)
 
+        if not allow_imm and select_imm:
+            raise AsmSyntaxError('Combination of operands not support')
         return args
 
     def _set_register(self, reg_nr, value):
@@ -182,156 +216,159 @@ class Q1Core:
 
     # === Below are Q1ASM opcode mnemonics with _ prefix.
 
+    @evaluate_args('')
     def _illegal(self):
         raise Illegal('illegal instruction')
 
+    @evaluate_args('')
     def _stop(self):
         raise Halt('stop instruction')
 
+    @evaluate_args('')
     def _nop(self):
         self.clock.add_ticks(1)
 
-    @evaluate_args
+    @evaluate_args('IRL')
     def _jmp(self, label):
         self.clock.add_ticks(4)
         self.iptr = label
 
-    @evaluate_args
+    @evaluate_args('R,I,IRL')
     def _jlt(self, register, n, label):
         self.clock.add_ticks(4)
         if register < n:
             self.iptr = label
 
-    @evaluate_args
+    @evaluate_args('R,I,IRL')
     def _jge(self, register, n, label):
         self.clock.add_ticks(4)
         if register >= n:
             self.iptr = label
 
-    @evaluate_args(dest_arg=0)
+    @evaluate_args('R,IRL', dest_arg=0)
     def _loop(self, register, label):
         self.clock.add_ticks(5)
         self._set_register(register, self.R[register] - 1)
         if self.R[register] != 0:
             self.iptr = label
 
-    @evaluate_args(dest_arg=1)
+    @evaluate_args('IRL,R', dest_arg=1)
     def _move(self, source, destination):
         self.clock.add_ticks(1)
         self._set_register(destination, source)
 
-    @evaluate_args(dest_arg=1)
+    @evaluate_args('IR,R', dest_arg=1)
     def _not(self, source, destination):
         self.clock.add_ticks(2)
         self._set_register(destination, ~source)
 
-    @evaluate_args(dest_arg=2)
+    @evaluate_args('R,IR,R', dest_arg=2)
     def _add(self, lhs, rhs, destination):
         self.clock.add_ticks(2)
         self._set_register(destination, lhs + rhs)
 
-    @evaluate_args(dest_arg=2)
+    @evaluate_args('R,IR,R', dest_arg=2)
     def _sub(self, lhs, rhs, destination):
         self.clock.add_ticks(2)
         self._set_register(destination, lhs - rhs)
 
-    @evaluate_args(dest_arg=2)
+    @evaluate_args('R,IR,R', dest_arg=2)
     def _and(self, lhs, rhs, destination):
         self.clock.add_ticks(2)
         self._set_register(destination, lhs & rhs)
 
-    @evaluate_args(dest_arg=2)
+    @evaluate_args('R,IR,R', dest_arg=2)
     def _or(self, lhs, rhs, destination):
         self.clock.add_ticks(2)
         self._set_register(destination, lhs | rhs)
 
-    @evaluate_args(dest_arg=2)
+    @evaluate_args('R,IR,R', dest_arg=2)
     def _xor(self, lhs, rhs, destination):
         self.clock.add_ticks(2)
         self._set_register(destination, lhs ^ rhs)
 
-    @evaluate_args(dest_arg=2)
+    @evaluate_args('R,IR,R', dest_arg=2)
     def _asl(self, lhs, rhs, destination):
         self.clock.add_ticks(2)
         self._set_register(destination, lhs << rhs)
 
-    @evaluate_args(dest_arg=2)
+    @evaluate_args('R,IR,R', dest_arg=2)
     def _asr(self, lhs, rhs, destination):
         self.clock.add_ticks(2)
         self._set_register(destination, lhs >> rhs)
 
-    @evaluate_args
+    @evaluate_args('IR')
     def _set_mrk(self, value):
         self.clock.add_ticks(1)
         self.renderer.set_mrk(value)
 
-    @evaluate_args
+    @evaluate_args('')
     def _reset_ph(self):
         self.clock.add_ticks(1)
         self.renderer.reset_ph()
 
-    @evaluate_args
+    @evaluate_args('IR,IR,IR')
     def _set_ph(self, arg0, arg1, arg2):
         self.clock.add_ticks(1)
         self.renderer.set_ph(arg0, arg1, arg2)
 
-    @evaluate_args
+    @evaluate_args('IR,IR,IR')
     def _set_ph_delta(self, arg0, arg1, arg2):
         self.clock.add_ticks(1)
         self.renderer.set_ph_delta(arg0, arg1, arg2)
 
-    @evaluate_args
+    @evaluate_args('IR,IR')
     def _set_awg_gain(self, gain0, gain1):
         self.clock.add_ticks(1)
         self.renderer.set_awg_gain(gain0, gain1)
 
-    @evaluate_args
+    @evaluate_args('IR,IR')
     def _set_awg_offs(self, offset0, offset1):
         self.clock.add_ticks(1)
         self.renderer.set_awg_offs(offset0, offset1)
 
-    @evaluate_args
+    @evaluate_args('I')
     def _upd_param(self, wait_after):
         self.clock.add_ticks(1)
         self.clock.schedule_rt(self.renderer.time)
         self.renderer.upd_param(wait_after)
 
-    @evaluate_args
+    @evaluate_args('IR,IR,I')
     def _play(self, wave0, wave1, wait_after):
         self.clock.add_ticks(1)
         self.clock.schedule_rt(self.renderer.time)
         self.renderer.play(wave0, wave1, wait_after)
 
-    @evaluate_args
+    @evaluate_args('I,IR,I')
     def _acquire(self, bins, bin_index, wait_after):
         self.clock.add_ticks(1)
         self.clock.schedule_rt(self.renderer.time)
         self.renderer.acquire(bins, bin_index, wait_after)
 
-    @evaluate_args
+    @evaluate_args('I,IR,R,R,I')
     def _acquire_weighed(self, bins, bin_index, weight0, weight1, wait_after):
         self.clock.add_ticks(1)
         self.clock.schedule_rt(self.renderer.time)
         self.renderer.acquire_weighed(bins, bin_index, weight0, weight1, wait_after)
 
-    @evaluate_args
+    @evaluate_args('IR')
     def _wait(self, time):
         self.clock.add_ticks(1)
         self.clock.schedule_rt(self.renderer.time)
         self.renderer.wait(time)
 
-    @evaluate_args
+    @evaluate_args('IR')
     def _wait_sync(self, wait_after):
         # assume wait_sync pauses the RT exec for at least 200 ns = 50 ticks
         self.clock.add_ticks(-50)
         self.renderer.wait_sync(wait_after)
 
-    @evaluate_args
+    @evaluate_args('IR')
     def _wait_trigger(self, wait_after):
         raise NotImplementedError()
 
-    @evaluate_args
-    def _sw_req(self, wait_after):
+    @evaluate_args('IR')
+    def _sw_req(self, value):
         raise NotImplementedError()
 
 
