4b8d5827e9fa8137e3825d10801e88dc5d93c419
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2022-02-25 10:45:58 +0100

    Return system status in correct format

--- q1simulator/q1simulator.py
+++ q1simulator/q1simulator.py
@@ -53,7 +53,7 @@ class Q1Simulator(qc.Instrument):
             logging.info(f'{self.name}:{name}={value}')
 
     def get_system_status(self):
-        return 'OK'
+        return {'status':'OK', 'flags':[]}
 
     def arm_sequencer(self, seq_nr):
         self.armed_seq.add(seq_nr)
