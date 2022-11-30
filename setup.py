b35b2d92def4ff4536dc828420b58413135261b1
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2022-11-30 12:08:26 +0100

    Increment version to get aligned with qblox-instruments version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ packages = ['q1simulator']
 print('packages: {}'.format(packages))
 
 setup(name="q1simulator",
-	version="0.4.6",
+	version="0.6.0",
     description="Simulator for Q1ASM",
     author="Sander de Snoo",
 	packages = find_packages(),
