a7488ceecc8e6e3fa73a332e51719c7fa0951208
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2022-12-02 15:34:11 +0100

    Bump version: 0.6.0 → 0.7.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ packages = ['q1simulator']
 print('packages: {}'.format(packages))
 
 setup(name="q1simulator",
-	version="0.6.0",
+	version="0.7.0",
     description="Simulator for Q1ASM",
     author="Sander de Snoo",
 	packages = find_packages(),
