3a7fbb45080d83d2bba6104ac1cd1e3904ebfd06
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2023-02-08 13:57:40 +0100

    Bump to 0.8.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ packages = ['q1simulator']
 print('packages: {}'.format(packages))
 
 setup(name="q1simulator",
-	version="0.8.1",
+	version="0.8.2",
     description="Simulator for Q1ASM",
     author="Sander de Snoo",
 	packages = find_packages(),
