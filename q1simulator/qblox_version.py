0e1fd5d905ac4f5588d0778f9c101c638af5d5d5
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2023-07-21 16:04:03 +0200

    Simulates v0.9 correctly.

--- q1simulator/qblox_version.py
+++ q1simulator/qblox_version.py
@@ -5,7 +5,7 @@ from q1simulator import __version__ as q1simulator_version
 
 def check_qblox_instrument_version():
     if qblox_version >= Version('0.10'):
-        print(f'WARNING Q1Simulator {q1simulator_version} simulates qblox_instruments v0.8.x, '
+        print(f'WARNING Q1Simulator {q1simulator_version} simulates qblox_instruments v0.9.x, '
               f'but qblox_instruments version {qblox_version} is installed')
     elif qblox_version < Version('0.8'):
         raise Exception('Q1Simulator {q1simulator_version} expects qblox_instruments version v0.8+')
