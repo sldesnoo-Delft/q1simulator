- Added plotting of acquisition windows to Q1ProgramBrowser and `simulator.plot`. - Fixed units [kns] in Q1ProgramBrowser. - Added display of time at mouse cursor in Q1ProgramBrowser.

q1simulator/gui/program_plotter.py
@@ -1,8 +1,9 @@
-from numbers import Number
 from typing import Any
 
+import numpy as np
 import pyqtgraph as pg
-from qtpy import QtWidgets
+from numpy.typing import NDArray
+from qtpy import QtWidgets, QtCore
 
 from q1simulator import Q1Simulator
 from q1simulator.channel_data import MarkerOutput, SampledOutput
@@ -48,9 +49,14 @@ class PlotWindow(QtWidgets.QMainWindow):
 
         pw = pg.PlotWidget()
         self._plot_widget = pw
+        pw.getAxis('bottom').enableAutoSIPrefix(False)
         pw.getAxis('bottom').setLabel("time", "ns")
         pw.getAxis('left').setLabel("output", "V")
         pw.addLegend()
+        self.proxy = pg.SignalProxy(pw.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
+        self._time_label = pg.LabelItem("-- ns")
+        self._time_label.setParentItem(pw.getPlotItem().vb)
+        self._time_label.anchor(itemPos=(0.0, 0.0), parentPos=(0.0, 0.0), offset=(35, 10))
 
         layout = QtWidgets.QHBoxLayout()
         layout.addWidget(self._plot_widget, 1)
@@ -58,10 +64,22 @@ class PlotWindow(QtWidgets.QMainWindow):
         content.setLayout(layout)
         self.setCentralWidget(content)
 
+    def mouseMoved(self, evt):
+        viewbox = self._plot_widget.getPlotItem().vb
+        pos = evt[0]  # using signal proxy turns original arguments into a tuple
+        if viewbox.sceneBoundingRect().contains(pos):
+            mousePoint = viewbox.mapSceneToView(pos)
+            x_val = mousePoint.x()
+            self._time_label.setText(f"{x_val:.0f} ns")
+
     def plot(self, x, y, style: str = "", label: str = None):
         color = color_list[self._n_plots % len(color_list)]
+        if style == ":":
+            pen = pg.mkPen(color, style=QtCore.Qt.DashLine)
+        else:
+            pen = color
         pw = self._plot_widget
-        pw.plot(x, y, name=label, pen=color)
+        pw.plot(x, y, name=label, pen=pen)
         self._n_plots += 1
 
     def closeEvent(self, event):
@@ -79,6 +97,8 @@ def plot_simulation(
         render_repetitions: bool = False,
         skip_wait_sync: bool = True,
         analogue_output_frequency: float = 4e9,
+        plot_i_only: bool = True,
+        trace: bool = False,
         ):
     global _sim_counter
 
@@ -94,7 +114,7 @@ def plot_simulation(
     sim.config("max_core_cycles", max_core_cycles)
     sim.config('skip_loops', () if render_repetitions else ("_start", ))
     sim.config('skip_wait_sync', skip_wait_sync)
-    # sim.config('trace', True)
+    sim.config('trace', trace)
     sim.ignore_triggers = True
 
     _sim_counter += 1
@@ -126,13 +146,13 @@ def plot_simulation(
 
     for i, ch_name in enumerate(channels):
         status = sim.get_sequencer_status(i)
-        print(f"State {ch_name}: {status.status} - {status.state}, {[str(flag) for flag in status.info_flags]}")
         if status.warn_flags or status.err_flags:
+            print(f"State {ch_name}: {status.status} - {status.state}, {[str(flag) for flag in status.info_flags]}")
             print(f"  warnings: {[str(flag) for flag in status.warn_flags]}, "
                   f"errors: {[str(flag) for flag in status.err_flags]}, log: {status.log}")
 
     # plot
-    output_per_sequencer = True
+    output_per_sequencer = True # TODO  @@@
     output = sim.get_output(
         t_min=min_time,
         t_max=max_time,
@@ -154,10 +174,39 @@ def plot_simulation(
             pw.plot(x, y, linestyle, label=name)
         elif isinstance(data, SampledOutput):
             t = data.get_time_data()
+            if plot_i_only and name.endswith("-Q"):
+                continue
             pw.plot(t, data.data, linestyle, label=name)
 
-    # sim.print_acquisitions()
+    acq_windows = sim.get_acquisition_windows()
+    for name, data in acq_windows.items():
+        n = len(data)
+        t_list = [data[i][0] for i in range(n)]
+        i_list = [data[i][1] for i in range(n)]
+        q_list = [data[i][2] for i in range(n)]
+        t = _concat_with_NaN(t_list)
+        i = _concat_with_NaN(i_list)
+        q = _concat_with_NaN(q_list)
+        if not plot_i_only and not np.array_equal(i, q, equal_nan=True):
+            i_label = name+"-I"
+            q_label = name+"-Q"
+        else:
+            i_label = name
+            q = None
+        pw.plot(t, i, ":", label="ACQ:"+i_label)
+        if q is not None:
+            pw.plot(t, q, ":", label="ACQ:"+q_label)
 
     sim.close()
 
     return pw
+
+
+def _concat_with_NaN(x: list[NDArray]) -> NDArray:
+    nan_array = np.array([np.nan])
+    t = []
+    for i, d in enumerate(x):
+        if i > 0:
+            t.append(nan_array)
+        t.append(d)
+    return np.concatenate(t)

q1simulator/q1simulator.py
@@ -4,6 +4,7 @@ from functools import partial
 import numpy as np
 import matplotlib.pyplot as pt
 import qcodes as qc
+from numpy.typing import NDArray
 
 from .channel_data import MarkerOutput, SampledOutput
 from .q1sequencer import Q1Sequencer
@@ -88,13 +89,13 @@ class Q1Module(qc.instrument.InstrumentBase):
     # This class is used as a mixin. Although quite heavy mixin.
 
     def init_module(self, n_sequencers=6, sim_type=None):
-        
-        # When using the qblox_instruments ClusterType class, 
+
+        # When using the qblox_instruments ClusterType class,
         # for example ClusterType.CLUSTER_QCM,
         # the sim_type is formatted as "Cluster QCM".
         # So, start by stripping the "Cluster " prefix.
-        sim_type = str(sim_type).replace("Cluster ","")
-        
+        sim_type = str(sim_type).replace("Cluster ", "")
+
         self._sim_type = sim_type
         if sim_type is None:
             raise Exception('sim_type must be specified')
@@ -321,6 +322,8 @@ class Q1Module(qc.instrument.InstrumentBase):
                     t = data.get_time_data()
                     pt.plot(t, data.data, linestyle, label=name)
 
+        self._plot_acquisition_windows()
+
         limits = {}
         if t_min is not None:
             limits['left'] = t_min
@@ -328,6 +331,36 @@ class Q1Module(qc.instrument.InstrumentBase):
             limits['right'] = t_max
         if limits:
             pt.xlim(**limits)
+
+    def _plot_acquisition_windows(self, plot_i_only: bool = True):
+
+        def _concat_with_NaN(x: list[NDArray]) -> NDArray:
+            nan_array = np.array([np.nan])
+            t = []
+            for i, d in enumerate(x):
+                if i > 0:
+                    t.append(nan_array)
+                t.append(d)
+            return np.concatenate(t)
+
+        acq_windows = self.get_acquisition_windows()
+        for name, data in acq_windows.items():
+            n = len(data)
+            t_list = [data[i][0] for i in range(n)]
+            i_list = [data[i][1] for i in range(n)]
+            q_list = [data[i][2] for i in range(n)]
+            t = _concat_with_NaN(t_list)
+            i = _concat_with_NaN(i_list)
+            q = _concat_with_NaN(q_list)
+            if not plot_i_only and not np.array_equal(i, q, equal_nan=True):
+                i_label = name+"-I"
+                q_label = name+"-Q"
+            else:
+                i_label = name
+                q = None
+            pt.plot(t, i, ":", label="ACQ:"+i_label)
+            if q is not None:
+                pt.plot(t, q, ":", label="ACQ:"+q_label)
 
     def get_output(self,
                    t_min: float = None,
@@ -408,6 +441,14 @@ class Q1Module(qc.instrument.InstrumentBase):
 
     def print_registers(self, seq_nr, reg_nrs=None):
         self.sequencers[seq_nr].print_registers(reg_nrs)
+
+    def get_acquisition_windows(self) -> dict[str, list[tuple[NDArray, NDArray, NDArray]]]:
+        windows = {}
+        for seq in self.sequencers:
+            seq_windows = seq.get_acquisition_windows()
+            if seq_windows:
+                windows[seq.label] = seq_windows
+        return windows
 
 
 class Q1Simulator(qc.Instrument, Q1Module):

q1simulator/rt_renderer.py
@@ -7,6 +7,7 @@ from collections.abc import Sequence as AbcSequence
 from functools import wraps
 
 import numpy as np
+from numpy.typing import NDArray
 
 from .analogue_filter import AnalogueFilter
 from .channel_data import MarkerOutput, SampledOutput
@@ -37,6 +38,14 @@ class AcqConf:
     trigger_en: bool = False
     trigger_addr: int = 0
     trigger_invert: bool = False
+
+
+@dataclass
+class AcqIntegration:
+    start: int
+    stop: int
+    weight0: int | None = None
+    weight1: int | None = None
 
 
 def _phase2float(phase_uint32):
@@ -118,6 +127,7 @@ class Renderer:
         self.waves = (None, None)
         self.out0 = []
         self.out1 = []
+        self.acq_integrations: list[AcqIntegration] = []
         self.marker_out = [list() for _ in range(4)]  # a list per marker
         self.acq_times = {i: [] for i in self.acquisitions}
         self.acq_buffer = AcqBuffer()
@@ -252,7 +262,7 @@ class Renderer:
                 len(self.acq_weights[weight0]),
                 len(self.acq_weights[weight1])
             )
-            self._add_acquisition(acq_index, bin_index, duration)
+            self._add_acquisition(acq_index, bin_index, duration, weight0, weight1)
         self._render(wait_after)
 
     @check_conditional(clear_latched_settings=True)
@@ -529,12 +539,17 @@ class Renderer:
         self.acq_data[index] = np.full((num_bins, 2), np.nan)
         self.acq_thresholded[index] = np.full(num_bins, np.nan)
 
-    def _add_acquisition(self, acq_index, bin_index, duration):
+    def _add_acquisition(self, acq_index: int, bin_index: int, duration: int,
+                         weight0: int | None = None,
+                         weight1: int | None = None):
         t = self.time
+        t_end = t + duration
         if acq_index not in self.acquisitions:
             self._error('ACQ INDEX INVALID')
             return
+
         self.acq_times[acq_index].append((t, bin_index))
+        self.acq_integrations.append(AcqIntegration(t, t_end, weight0, weight1))
         if bin_index >= self.acquisitions[acq_index]:
             self._error('ACQ BIN INDEX INVALID')
             return
@@ -557,7 +572,6 @@ class Renderer:
         self.acq_count[acq_index][bin_index] += 1
 
         if acq_conf.trigger_en:
-            t_end = t + duration
             if self.acq_trigger_value is None:
                 trigger_state = state ^ acq_conf.trigger_invert
             else:
@@ -572,6 +586,7 @@ class Renderer:
             self._error('ACQ INDEX INVALID')
             return
         self.acq_times[acq_index].append((start, bin_index))
+        self.acq_integrations.append(AcqIntegration(start, stop))
         if bin_index >= self.acquisitions[acq_index]:
             self._error('ACQ BIN INDEX INVALID')
             return
@@ -668,6 +683,29 @@ class Renderer:
 
     def get_acquisition_list(self):
         return self.acq_times
+
+    def get_acquisition_windows(self) -> list[tuple[NDArray, NDArray, NDArray]]:
+        """Return acquisition integration windows.
+
+        Returns:
+            list with (time, I-window, Q-window)
+        """
+        windows = []
+        for acq_int in self.acq_integrations:
+            # Add 0,0 before and after window
+            t = np.arange(acq_int.start-1, acq_int.stop+1)
+            i = np.zeros(len(t))
+            q = np.zeros(len(t))
+            if acq_int.weight0 is not None:
+                w0 = self.acq_weights[acq_int.weight0]
+                w1 = self.acq_weights[acq_int.weight1]
+                i[1: len(w0)+1] = w0
+                q[1: len(w1)+1] = w1
+            else:
+                i[1: -1] = 1
+                q[1: -1] = 1
+            windows.append((t, i, q))
+        return windows
 
 
 class AcqBuffer: